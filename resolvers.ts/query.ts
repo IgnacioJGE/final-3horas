7a21b04d84ab669976ccc033bbf7a986711d8169
Ignacio García <ijgarciaest@gmail.com>
2024-07-03 14:40:01 +0200

    modified:   resolvers.ts/query.ts

--- resolvers.ts/query.ts
+++ resolvers.ts/query.ts
@@ -24,5 +24,23 @@ export const Query={
         }
         return arraymostrar
 
+    },
+    getContac: async(_:unknown,
+        args:{id:string}
+    ):Promise<Contacto>=>{
+        const contactomostrar= await Modelocontacto.findById(args.id)
+        if(!contactomostrar)throw new GraphQLError(`No se encuentra contacto con la id:${args.id}`)
+            const latlon:latlon= await getlatitudelongitude(contactomostrar.capital)
+        const hora= await gethora(latlon.latitude,latlon.longitude)
+        return ({   
+
+            id:contactomostrar._id,
+            Nombre:contactomostrar.Nombre,
+            apellidos:contactomostrar.apellidos,
+            phone:contactomostrar.phone,
+            pais:contactomostrar.pais,
+            hora:hora
+      
+    })
     }
 }
